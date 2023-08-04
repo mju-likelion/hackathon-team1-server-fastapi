f00f42b4982c205935ec24035b915d96592b5d01
k013600 <k012123600@gmail.com>
2023-08-04 23:02:55 +0900

    fix: 파일 위치 수정

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
-from nlp_task import detect_language
+from app.nlp_task import detect_language
 from typing import Optional
 
 class Text_to_detect(BaseModel):
@@ -21,7 +21,7 @@ app.add_middleware(
 
 
 
-@app.post("/fa/detect_language/")
+@app.post("/fa/detect-language/")
 def echo_text(text_to_detect:Text_to_detect):
     print(text_to_detect.text)
     language = detect_language(text_to_detect.text)
