ea69bb89ed0c719f6b96a0a5434cbe6a8abbd768
k013600 <k012123600@gmail.com>
2023-08-07 07:06:20 +0900

    feat: 데이터셋 db에 넣기 기능 구현

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
-from nlp_task import detect_language
+from nlp_task import detect_language, recommend_insurances
+from data_preprocessing import data_preprocessing
 from typing import Optional
+
 
 class Text_to_detect(BaseModel):
     text: str
     
+class Text_to_recommend(BaseModel):
+    text: str
 app = FastAPI()
 
 # CORS 설정
@@ -18,7 +22,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#언어 감지 기능
 @app.post("/detect-language")
-def echo_text(text_to_detect:Text_to_detect):
-    language = detect_language(text_to_detect.text)
+async def echo_text(text_to_detect:Text_to_detect):
+    language = await detect_language(text_to_detect.text)
     return { "language" : language }
+
+#보험 상품 추천 기능
+@app.post("/recommendations")
+async def insurances_recommend(text_to_recommend: Text_to_recommend):
+    insurance_ids = await recommend_insurances(text_to_recommend.text)
+    return insurance_ids
+
+#보험 정보 db에 입력 기능
+@app.post("/create-insurances")
+async def create_insurances(file: UploadFile = File(...)):
+    #파일 내용 읽기
+    insurance_data = await file.read()
+    response = await data_preprocessing(insurance_data)
+    return response
--- nlp_task.py
+++ nlp_task.py
@@ -3,7 +3,7 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import text
 
 
-def detect_language(input_text):
+async def detect_language(input_text):
     # STEP 2: Create a LanguageDetector object.
     base_options = python.BaseOptions(model_asset_path="./models/detector.tflite")
     options = text.LanguageDetectorOptions(base_options=base_options)
@@ -16,3 +16,8 @@ def detect_language(input_text):
     # STEP 4: Process the detection result and return the language code.
     for detection in detection_result.detections:
         return detection.language_code
+
+async def recommend_insurances(questions):
+    #개발중입니다.
+    ids = []
+    return ids
