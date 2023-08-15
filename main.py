ee9496ea1eba67d1b50b552ef676d8a86bc9fd40
k013600 <k012123600@gmail.com>
2023-08-16 05:55:25 +0900

    fix: cors 수정

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
