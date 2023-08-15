522e3bcc1653df86f18b5b98118d692d1d2bd7c2
k013600 <k012123600@gmail.com>
2023-08-16 05:53:53 +0900

    feat: 태깅 및 필터링 로직 추가

--- nlp_task.py
+++ nlp_task.py
@@ -1,7 +1,7 @@
 # STEP 1: Import the necessary modules.
 from mediapipe.tasks import python
 from mediapipe.tasks.python import text
-
+from filtering import extract_info_from_tags  
 
 async def detect_language(input_text):
     # STEP 2: Create a LanguageDetector object.
@@ -19,5 +19,7 @@ async def detect_language(input_text):
 
 async def recommend_insurances(questions):
     #개발중입니다.
+    tags = extract_info_from_tags(questions)
+    
     ids = ["6d63fb72-fe0a-4a45-8e16-5b61dbf586c1", "819d6360-9ed2-4bfe-a0c3-dfdabfa62924", "7307918d-c580-43f5-8c12-19d514a49937", "b655dbd0-32bf-48a2-a938-ae812d18886b"]
     return ids
